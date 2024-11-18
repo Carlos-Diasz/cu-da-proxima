e7712a93428ee29fbf6e688c81a92a76b72e1b82
Carlos Dias <carlos.dias.o.n@gmail.com>
2024-11-17 21:51:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cu-da-proxima
+ 
---
+++ pergunta.js
@@ -0,0 +1,15 @@
+function sim() {
+    alert("PREPARE O CU HIHIHIHIHI");
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
